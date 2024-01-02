6a1c6daa788819a7a0f4bb043ff53a3692c06c4a
Arthur Rosa <arosa420194@gmail.com>
2024-01-02 14:04:45 -0500

    Succesfully Processed Videos Locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,41 @@
 import express from "express";
+import ffmeg from "fluent-ffmpeg"
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-// "/" means the root route.
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+    // Get path of the input video file from the request body.
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    // Check if the file path is defined.
+    if (!inputFilePath) {
+        res.status(400).send("Bad Request: Missing input file path.");
+    }
+
+    if (!outputFilePath) {
+        res.status(400).send("Bad Request: Missing output file path.");
+    }
+
+    // Create the ffmpeg command.
+    // Configures input path to 360p and sends message when finished.
+    // If an error occured send error message.
+    ffmeg(inputFilePath)
+        .outputOption("-vf", "scale=-1:360")
+        .on("end", () => {
+            console.log('Processing finished successfully');
+            res.status(200).send('Processing finished successfully');
+        })
+        .on("error", function(err: any) {
+            console.log('An error occured: ' + err.message);
+            res.status(500).send('An error occured: ' + err.message);
+        })
+        .save(outputFilePath);
+
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
